69209645a8b6b9dafa88965f345082a9448a8ab7
Barry1208 <79997975+Barry1208@users.noreply.github.com>
2022-04-22 20:36:55 -0400

    stage2

---
+++ login/login.js
@@ -0,0 +1,13 @@
+function login(){
+    var username = document.getElementById("Username")
+    var password = doucment.getElementById("Password")
+    if(username.value==""){
+        alert("Username");
+    }
+    else if (password.value==""){
+        alert("Password");
+    }
+    else{
+        alert("welcome");
+    }
+}
